aab9a922ba4ec02777b082b9e890253e9e231705
Keth Brings <keith.brings@noizu.com>
2025-04-25 23:58:19 +0700

    setting up document content type

---
+++ lib/vnext_genai/nodes/message/content/document_content.ex
@@ -0,0 +1,25 @@
+#content.type document
+#content.cache_control.type
+#content.citation
+#content.context
+#content.title
+#role
+#
+#pdf
+#content.source.data
+#content.source.media_type application/pdf
+#content.source.type base64
+#
+#
+#text
+#  content.source.data
+#  content.source.type text
+#  content.source.media_type text/plain
+#
+#content_block
+#content.source.type content
+#content.source.content:  string | array[content]
+#
+#URLPDFSource
+#content.source.type: url
+#content.source.url: string
