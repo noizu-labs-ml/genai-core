336f70315b032592248541f514af4f856d55171a
noizu <keith.brings@noizu.com>
2025-04-28 19:19:20 +0700

    vsn bump

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule GenAICore.MixProject do
       name: "GenAI Core",
       description: description(),
       package: package(),
-      version: "0.1.1",
+      version: "0.2.0",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
