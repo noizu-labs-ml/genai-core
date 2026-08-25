888f532a91761c597bafa510b39cfff138d2448e
Keith Brings <keith.brings@noizu.com>
2026-08-26 04:22:43 +0700

    vsn bump

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule GenAICore.MixProject do
       name: "GenAI Core",
       description: description(),
       package: package(),
-      version: "0.3.3",
+      version: "0.3.4",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
